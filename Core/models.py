ff3094533233b1146889d6884f5548b81063f0aa
Juan C Rosero <57236205+JuanK120@users.noreply.github.com>
2022-02-20 10:46:35 -0500

    Feature/models (#3)
    * creation of app Core and the  models to be used in the bd
    
    * change to models

---
+++ Core/models.py
@@ -0,0 +1,34 @@
+from pyexpat import model
+from django.db import models
+
+# Create your models here.
+
+
+class User(models.Model):
+    firstName = models.CharField(max_length=50)
+    lastName = models.CharField(max_length=50)
+    docType = models.CharField(max_length=50,null=False)
+    docNumber = models.CharField(max_length=50,null=False)
+    role = models.IntegerField()
+    email = models.EmailField()
+    password = models.CharField(max_length=50)
+
+class BankTeller(models.Model):
+    name = models.CharField(max_length=50)
+    
+class Service(models.Model):
+    service_id = models.BigAutoField(primary_key=True)
+    serviceName = models.CharField(max_length=50)
+    description = models.TextField()
+    serviceType = models.IntegerField()
+    teller_id = models.ForeignKey(BankTeller, on_delete=models.CASCADE,null=True)
+
+class Ticket(models.Model):
+    orderNumber = models.IntegerField()
+    userId = models.ForeignKey(User,on_delete=models.CASCADE)
+    serviceId = models.ForeignKey(Service,on_delete=models.CASCADE)
+    state = models.IntegerField()
+    arrivalDate = models.DateField(auto_now_add=True)
+    arrivalTime = models.TimeField(auto_now_add=True)
+
+
