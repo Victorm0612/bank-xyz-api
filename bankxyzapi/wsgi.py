048592d014c5ec3f7f0fbcd93f69275003e64912
victorm0612 <victor.manuel.vargas@correounivalle.edu.co>
2022-02-21 19:36:29 -0500

    new settings created

--- bankxyzapi/wsgi.py
+++ bankxyzapi/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bankxyzapi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bankxyzapi.settings.base')
 
 application = get_wsgi_application()
